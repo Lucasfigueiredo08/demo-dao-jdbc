26a048a5ab8ff40cb91430ad2113b94b403091df
Lucas Figueiredo <lukas.liberato14@gmail.com>
2024-04-11 18:27:25 -0300

    SelleDaoJDBC and DaoFactory

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,5 +1,7 @@
 package org.example.application;
 
+import org.example.model.dao.DaoFactory;
+import org.example.model.dao.SellerDao;
 import org.example.model.entities.Department;
 import org.example.model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
         Department department = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "3tqFP@example.com", new Date(), 3000.0, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
 
