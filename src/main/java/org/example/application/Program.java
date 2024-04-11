1099a7213704e2aa97f1be956db1464eeefce537
Lucas Figueiredo <lukas.liberato14@gmail.com>
2024-04-11 18:17:45 -0300

    Seller entity class

--- src/main/java/org/example/application/Program.java
+++ src/main/java/org/example/application/Program.java
@@ -1,6 +1,9 @@
 package org.example.application;
 
 import org.example.model.entities.Department;
+import org.example.model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -9,7 +12,9 @@ public class Program {
 
         Department department = new Department(1, "Books");
 
-        System.out.println(department);
+        Seller seller = new Seller(21, "Bob", "3tqFP@example.com", new Date(), 3000.0, department);
+
+        System.out.println(seller);
 
     }
 }
